1bf79b9f07e7c671e5a31f30102e66131e26f384
Embedded System User <user@example.com>
2026-05-22 10:08:55 +0200

    test programm

---
+++ esp32_blink.ino
@@ -0,0 +1,15 @@
+// Simple ESP32 blink sketch
+// Uses the built-in LED on most ESP32 boards (GPIO 2)
+
+void setup()
+{
+    pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
+}
